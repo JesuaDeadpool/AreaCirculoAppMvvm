ada4b17ace39b16b9ae83c5c9bfecbbc45d8f9ae
Jesua <jesuaduron@gmail.com>
2025-03-25 19:26:09 -0600

    Crear el diseño en el AreaCirculoView

--- AreaCirculoAppMvvm/View/AreaCirculoView.xaml.cs
+++ AreaCirculoAppMvvm/View/AreaCirculoView.xaml.cs
@@ -1,9 +1,15 @@
+using AreaCirculoAppMvvm.ViewModel;
+
 namespace AreaCirculoAppMvvm.View;
 
 public partial class AreaCirculoView : ContentPage
 {
+
+	AreaCirculoViewModel viewModel; 
 	public AreaCirculoView()
 	{
 		InitializeComponent();
+		viewModel = new AreaCirculoViewModel();
+		BindingContext = viewModel; 
 	}
 }
